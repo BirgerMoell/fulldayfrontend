1b602f6218fa9e005f8d63cc706be07c65cc0962
Birger Moëll <birger.moell@gmail.com>
2019-11-13 23:02:11 +0100

    Updates

---
+++ precache-manifest.d650d9d2aae3ad18dd00cf06717df4c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9314cbe0dcb612d7c1ec0aafec25382",
+    "url": "/fulldayfrontend/index.html"
+  },
+  {
+    "revision": "3bdc15f02809f600aa9c",
+    "url": "/fulldayfrontend/static/css/main.2c0588a6.chunk.css"
+  },
+  {
+    "revision": "98c980c13ceb3fcfef03",
+    "url": "/fulldayfrontend/static/js/2.05054e9c.chunk.js"
+  },
+  {
+    "revision": "3bdc15f02809f600aa9c",
+    "url": "/fulldayfrontend/static/js/main.d0d17bf3.chunk.js"
+  },
+  {
+    "revision": "15ebbe692165f727205a",
+    "url": "/fulldayfrontend/static/js/runtime-main.318506af.js"
+  }
+]);
